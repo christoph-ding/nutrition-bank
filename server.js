b2b397a751de9aa35f82ee1be2379f9af4299b3b
Pokegear <christoph.ding@gmail.com>
2017-01-02 15:05:04 -0500

    Refactor server to have some helper functions.  Add morgan logging.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var path = require('path');
+var morgan = require('morgan');
 
 var app = express();
+app.use(morgan());
 
 const PORT = 8000;
 
@@ -10,12 +12,19 @@ app.listen(PORT, function() {
 })
 
 app.get("/", function(req, res) {
-  var pathToHtml = path.join(__dirname, 'index.html');
-  res.sendFile(pathToHtml);
+  res.sendFile(fullFilePath('index.html'));
+})
+
+app.get("/src/index.js", function(req, res) {  
+  res.sendFile(fullFilePath('src/index.js'));
 })
 
 // helper functions
 var logHelpfulInfo = function () {
   console.log("Hi, I am running on port:", PORT);
   console.log("__dirname is;", __dirname);  
+}
+
+var fullFilePath = function(fileName) {
+  return path.join(__dirname, fileName);
 }
