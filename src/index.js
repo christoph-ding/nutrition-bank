6845a1a711a57a0ba6edf600b8e2c56aeb497ff2
Pokegear <christoph.ding@gmail.com>
2017-01-04 13:27:37 -0500

    Make stubs for all the components that will eventually make the app.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,52 @@
-console.log('loaded 1 index.js');
+console.log('loaded index.js');
 
 // the entire app
 class NutritionBankApp extends React.Component {
+  // constructor
+
+
+  // get data
+
+  // mount for data
+
+    // did mount
+
+    // will unmount
+
+    // component will update? 
+
+  // update render to have all the embedded components
   render(){
     return (
-      <h1> i am a component </h1>
+      <div className="app">
+        <h1> I am the entire app </h1>
+      </div>
     );
   }
 }
 
+// title
+const Title = (<h1> Title </h1>);
+
+// header buttons area
+class HeaderButtons extends React.Component {
+
+}
+
+// a single button
+class SortButton extends React.Component {
+
+}
+
+// nutrition cards area
+class NutritionBank extends React.Component {
+
+}
+
+// individual food card
+const NutritionCard = (<h2> Card </h2>);
+
+// render elements
 ReactDOM.render(
   <NutritionBankApp />,
   document.getElementById('root')
