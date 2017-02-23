675b39c472b8261144c8578d12661aedf416ddfa
ironpup <christoph.ding@gmail.com>
2017-02-23 15:10:06 -0500

    Save React and React-dom as dependencies.  App now renders react.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 'use strict';
+import React from 'react';
+import { render } from 'react-dom';
 
-class NutritionBankApp extends React.Componet {
+class NutritionBankApp extends React.Component {
   //constructor
   constructor(props) {
     super(props);
     this.state = {
-
     }
   }
 
@@ -17,7 +18,7 @@ class NutritionBankApp extends React.Componet {
 }
 
 // render elements
-ReactDOM.render(
+render(
   <NutritionBankApp />,
   document.getElementById('root')
 );
